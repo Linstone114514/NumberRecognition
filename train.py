2812efe0c5c520288b1a549e47334829c966b698
Linstone114514 <18250563237@163.com>
2025-08-10 22:37:54 +0800

    ver. 1.0 (25/8/10 2238)

--- train.py
+++ train.py
@@ -20,15 +20,23 @@ def load_mnist_from_raw(path):
 
 (x_train, y_train), (x_test, y_test) = load_mnist_from_raw("data")
 
-# 2. 模型
+#模型
 model = models.Sequential([
-    layers.Conv2D(32, 3, activation='relu', input_shape=(28,28,1)),
+    #1
+    layers.Conv2D(32, 3, padding='same', activation='relu', input_shape=(28,28,1)),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, activation='relu'),
+    layers.Dropout(0.25),
+    #2
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),  # 保持相同通道数
     layers.MaxPooling2D(),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(10, activation='softmax')
+    layers.Dropout(0.25),  # 添加正则化
+
+    layers.Flatten(),#展平
+    layers.Dropout(0.5),
+    layers.Dense(128, activation='relu'),#全连接
+    layers.Dense(10, activation='softmax')#输出
 ])
 model.compile(optimizer='adam',
               loss='categorical_crossentropy',
